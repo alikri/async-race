8912c98dda36049b7e9206cf9c6b32896caca677
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-20 22:09:38 +0700

    feat: add car component with svg image

--- async-race/src/components/car/Car.tsx
+++ async-race/src/components/car/Car.tsx
@@ -1,1 +1,15 @@
+import { useState } from 'react';
 import './car.styles.scss';
+import CarIcon from './CarIcon';
+
+const Car = () => {
+  const [color] = useState('black');
+
+  return (
+    <div className="car-wrapper">
+      <CarIcon color={color} />
+    </div>
+  );
+};
+
+export default Car;
--- async-race/src/pages/garage/Garage.tsx
+++ async-race/src/pages/garage/Garage.tsx
@@ -1,9 +1,11 @@
+import Car from '../../components/car/Car';
 import ControlPanel from '../../components/controlPanel/ControlPanel';
 
 const Garage = () => {
   return (
     <div className="garage-container">
       <ControlPanel />
+      <Car />
     </div>
   );
 };
