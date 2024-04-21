ba5f25be53959e07227f44e5528f294715a25074
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-21 13:57:48 +0700

    feat: update apiRequest to return data and headers

--- async-race/src/utils/apiRequest.ts
+++ async-race/src/utils/apiRequest.ts
@@ -6,21 +6,15 @@ interface RequestOptions {
   headers?: Record<string, string>;
 }
 
-interface APIResponse<T = any> {
-  data: T;
-  error?: string;
-}
-
+// makeApiRequest remains universal and also returns headers
 async function makeApiRequest<T>({
   endpoint,
   method = 'GET',
   body,
   params = {},
   headers = {},
-}: RequestOptions): Promise<APIResponse<T>> {
+}: RequestOptions): Promise<{ data: T; headers: Headers }> {
   const baseUrl = process.env.REACT_APP_API_URL as string;
-  // eslint-disable-next-line no-console
-  console.log(baseUrl);
   const url = new URL(endpoint, baseUrl);
 
   if (params instanceof URLSearchParams) {
@@ -41,18 +35,14 @@ async function makeApiRequest<T>({
     body: body && method !== 'GET' ? JSON.stringify(body) : null,
   };
 
-  try {
-    const response = await fetch(url.toString(), options);
-    if (!response.ok) {
-      const errorBody = await response.json();
-      throw new Error(errorBody.error || 'API request failed');
-    }
-    const responseData: T = await response.json();
-    return { data: responseData };
-  } catch (error) {
-    console.error('API request error:', error);
-    throw new Error(`Failed to fetch from ${endpoint}: ${error}`);
+  const response = await fetch(url.toString(), options);
+  if (!response.ok) {
+    const errorBody = await response.json();
+    throw new Error(errorBody.error || 'API request failed');
   }
+
+  const responseData: T = await response.json();
+  return { data: responseData, headers: response.headers };
 }
 
 export default makeApiRequest;
