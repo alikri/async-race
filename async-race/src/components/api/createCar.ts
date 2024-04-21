95a76725932384522366234de92ae79b0acfb6ef
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-21 17:04:15 +0700

    feat: add create car method

---
+++ async-race/src/components/api/createCar.ts
@@ -0,0 +1,30 @@
+import makeApiRequest from '../../utils/apiRequest';
+import { CarData } from '../car/Car';
+
+const createCar = async (name: string, color: string): Promise<CarData> => {
+  const endpoint = '/garage';
+  const method = 'POST';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const body = {
+    name,
+    color,
+  };
+
+  try {
+    const response = await makeApiRequest<CarData>({
+      endpoint,
+      method,
+      headers,
+      body,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to create car:', error);
+    throw error;
+  }
+};
+
+export default createCar;
