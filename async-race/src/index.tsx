699e210a91828629af6a07d935d5e3c0aaa76f69
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-22 20:27:00 +0700

    fix :fix error wit hmissing logo

--- async-race/src/index.tsx
+++ async-race/src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 import Layout from './components/layout/layout';
 import Garage from './pages/garage/Garage';
 import Winners from './pages/winners/Winners';
-import { CarProvider } from './context/carContext';
+import { CarProvider } from './context/carContext/carContext';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
