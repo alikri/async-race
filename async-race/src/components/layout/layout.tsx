c15b901083147baaf3871e31513b1bb1e3a09c55
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-28 13:42:09 +0700

    feat: add fetching data when app opens

--- async-race/src/components/layout/layout.tsx
+++ async-race/src/components/layout/layout.tsx
@@ -1,9 +1,20 @@
 import './layout.styles.scss';
-
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../header/header';
+import { AppDispatch } from '../../redux/store';
+import { fetchWinners } from '../../redux/features/winners/winnersSlice';
+import { fetchAndUpdateCars } from '../../redux/features/car/carAPI';
 
 const Layout = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchWinners());
+    dispatch(fetchAndUpdateCars());
+  }, [dispatch]);
+
   return (
     <section className="main-wrapper">
       <div>
