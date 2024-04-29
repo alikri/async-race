6406d71285dd785ff77809e441dbd8a852b89934
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-29 20:54:45 +0700

    refactor: remove redundant code

--- async-race/src/redux/features/car/carSelectors.ts
+++ async-race/src/redux/features/car/carSelectors.ts
@@ -1,11 +1,6 @@
 import store, { RootState } from '../../store';
 
 export const getAllCars = (state: RootState) => state.cars.cars;
-export const getTotalCount = (state: RootState) => state.cars.totalCount;
-export const getCarById = (state: RootState, carId: number) => {
-  const car = state.cars.cars.find(data => data.id === carId);
-  return car || null;
-};
 
 export const getCarFromState = (carId: number) => {
   const state = store.getState();
