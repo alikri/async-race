0d48d7d3fc8686b54fcb0eab17250d80a64536c6
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-29 23:30:42 +0700

    refactor: remove redundant code

--- async-race/src/components/navigation/Navigation.tsx
+++ async-race/src/components/navigation/Navigation.tsx
@@ -1,27 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { AppDispatch } from '../../redux/store';
 import './navigation.styles.scss';
-import { getAllCars } from '../../redux/features/car/carSelectors';
-import { resetRaceResults } from '../../redux/features/raceResults/raceResultsSlice';
-import { resetCarState } from '../../redux/features/driveSettings/driveSettingsThunks';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  const dispatch: AppDispatch = useDispatch();
-  const allCars = useSelector(getAllCars);
-  const handleRouteToWinners = () => {
-    dispatch(resetRaceResults());
-    allCars.forEach(car => {
-      dispatch(resetCarState(car.id));
-    });
-  };
   return (
     <nav className="nav-wrapper">
       <div className="nav-container">
         <button type="button" className="nav-link">
           <Link to="/">Garage</Link>
         </button>
-        <button type="button" className="nav-link" onClick={handleRouteToWinners}>
+        <button type="button" className="nav-link" disabled={false}>
           <Link to="winners">Winners</Link>
         </button>
       </div>
