c4c298b0fd95ab6d1362ba1a4db01804f52334af
Alina Nosovets <alinanosovets6849@gmail.com>
2024-04-29 14:17:08 +0700

    feat: add arrows sorting order

--- async-race/src/components/winnersTable/WinnersTable.tsx
+++ async-race/src/components/winnersTable/WinnersTable.tsx
@@ -1,7 +1,11 @@
 import './winnersTable.scss';
+import { useSelector } from 'react-redux';
 import { WinnerData } from '../../types';
 import { CarData } from '../car/Car';
 import WinnerRow from './winnersRow/WinnerRow';
+import arrowDown from '../images/icons/arrow-up.svg';
+import arrowUp from '../images/icons/arrow-down.svg';
+import { RootState } from '../../redux/store';
 
 interface WinnersTableProps {
   winners: WinnerData[];
@@ -9,15 +13,55 @@ interface WinnersTableProps {
 }
 
 const WinnersTable = ({ winners, cars }: WinnersTableProps) => {
+  const { sortOrder } = useSelector((state: RootState) => state.winnerSortingPaginationSlice);
   return (
     <table className="winners-table">
       <thead>
         <tr>
-          <th>Number</th>
+          <th>
+            <div className="table-header">
+              <div> Number </div>
+              {sortOrder === 'DESC' ? (
+                <div className="arrow-wrapper">
+                  <img src={arrowDown} alt="Arrow Down" />
+                </div>
+              ) : (
+                <div className="arrow-wrapper">
+                  <img src={arrowUp} alt="Arrow Up" />
+                </div>
+              )}
+            </div>
+          </th>
           <th>Car</th>
           <th>Name</th>
-          <th>Wins</th>
-          <th>Best Time (seconds)</th>
+          <th>
+            <div className="table-header">
+              <div> Wins </div>
+              {sortOrder === 'DESC' ? (
+                <div className="arrow-wrapper">
+                  <img src={arrowDown} alt="Arrow Down" />
+                </div>
+              ) : (
+                <div className="arrow-wrapper">
+                  <img src={arrowUp} alt="Arrow Up" />
+                </div>
+              )}
+            </div>
+          </th>
+          <th>
+            <div className="table-header">
+              <div> Best Time (seconds) </div>
+              {sortOrder === 'DESC' ? (
+                <div className="arrow-wrapper">
+                  <img src={arrowDown} alt="Arrow Down" />
+                </div>
+              ) : (
+                <div className="arrow-wrapper">
+                  <img src={arrowUp} alt="Arrow Up" />
+                </div>
+              )}
+            </div>
+          </th>
         </tr>
       </thead>
       <tbody>
